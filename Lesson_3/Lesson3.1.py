ad9d8ccd7989d41fc8953c06950f83b2dcfe5c50
Heorhii <1xcgdf@gmail.com>
2024-04-22 22:17:53 +0300

    Lesson2

---
+++ Lesson_3/Lesson3.1.py
@@ -0,0 +1,17 @@
+num1 = float(input("Введіть перше число: "))
+op = input("Операція (+, -, *, /): ")
+num2 = float(input("Введіть друге число: "))
+
+if op == '+':
+    print("Результат:", num1 + num2)
+elif op == '-':
+    print("Результат:", num1 - num2)
+elif op == '*':
+    print("Результат:", num1 * num2)
+elif op == '/':
+    if num2 != 0:
+        print("Результат:", num1 / num2)
+    else:
+        print("Помилка: ділення на нуль!")
+else:
+    print("Невідома операція!")

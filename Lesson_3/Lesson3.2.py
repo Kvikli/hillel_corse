54dde6a6244f50790ceb191e45b753e52f49dcdf
Heorhii <1xcgdf@gmail.com>
2024-04-24 16:03:25 +0300

    Lesson2

--- Lesson_3/Lesson3.2.py
+++ Lesson_3/Lesson3.2.py
@@ -1,2 +1,9 @@
-li = [1, 2, 3, 4, 5]
-print(li[-1:])
+def move_last_to_first(lst):
+    if len(lst) > 1:
+        last_element = lst.pop()
+        lst.insert(0, last_element)
+
+
+my_list = [1, 2, 3, 4, 5]
+move_last_to_first(my_list)
+print(my_list)
---
+++ Lesson_3/Lesson3.3.py
@@ -0,0 +1,16 @@
+def divide_list(input_list):
+    if len(input_list) == 0:
+        return [[], []]
+    elif len(input_list) % 2 == 0:
+
+        midpoint = len(input_list) // 2
+        return [input_list[:midpoint], input_list[midpoint:]]
+    else:
+
+        midpoint = len(input_list) // 2 + 1
+        return [input_list[:midpoint], input_list[midpoint:]]
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+divided_lists = divide_list(my_list)
+print(divided_lists)

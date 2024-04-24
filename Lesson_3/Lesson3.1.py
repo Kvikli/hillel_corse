9d9e97fe27668f80a668d82d9086a27a2d84b728
Heorhii <1xcgdf@gmail.com>
2024-04-24 16:11:21 +0300

    Lesson3

--- Lesson_3/Lesson3.1.py
+++ Lesson_3/Lesson3.1.py
@@ -2,6 +2,7 @@ num1 = float(input("Введіть перше число: "))
 op = input("Операція (+, -, *, /): ")
 num2 = float(input("Введіть друге число: "))
 
+
 if op == '+':
     print("Результат:", num1 + num2)
 elif op == '-':
--- Lesson_3/Lesson3.3.py
+++ Lesson_3/Lesson3.3.py
@@ -10,7 +10,6 @@ def divide_list(input_list):
         midpoint = len(input_list) // 2 + 1
         return [input_list[:midpoint], input_list[midpoint:]]
 
-
 my_list = [1, 2, 3, 4, 5, 6, 7]
 divided_lists = divide_list(my_list)
 print(divided_lists)

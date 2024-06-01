672b05aa53482e3b07d36e71ee26344be839377f
Heorhii <1xcgdf@gmail.com>
2024-06-01 18:12:34 +0300

    Lesson11

---
+++ Lesson_11/Lesson11.1.py
@@ -0,0 +1,28 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def prime_generator(end):
+    num = 2
+    while num <= end:
+        if is_prime(num):
+            yield num
+        num += 1
+
+# Тестування функції
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
